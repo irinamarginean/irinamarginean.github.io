f2880628489ef700e23633208dca4856fb561286
Irina Marginean <44980921+irinamarginean@users.noreply.github.com>
2023-12-27 03:01:45 +0200

    new

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".navbar-list");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
 
+hamburger.addEventListener("hover", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach( n => {
+
+document.querySelectorAll(".link").forEach( n => {
     n.addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
